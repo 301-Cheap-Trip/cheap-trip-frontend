ac5bce4516b5ba52fe429ba62b18776eced69804
MELANIE JOHNSTON <MELANIE.JOHNSTON.1@OUTLOOK.COM>
2023-02-03 14:06:11 +0000

    about links put in footer so they match height

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -21,10 +21,10 @@ class Profile extends Component {
               With a strong foundation in programming languages and experience working on projects in a team environment, Dominick is eager to bring his skills and enthusiasm to a new role as a software developer. He is confident in his ability to learn quickly and is dedicated to continuously improving his abilities to deliver high-quality software. -ChatGPT
             </Card.Text>
           </Card.Body>
-          <Card.Body>
+          <Card.Footer>
             <Card.Link href="https://github.com/Dommcat"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/dominickmartin/"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
-          </Card.Body>
+          </Card.Footer>
         </Card>
         <Card className="card" style={{ width: '20rem' }}>
           <Card.Img variant="top" src={melPhoto} />
@@ -35,10 +35,10 @@ class Profile extends Component {
               also building things from scratch. I started a web developer course on Udemy and it seemed like the perfect fit. Although I am looking forward to all the aspects of web development, I really want to focus as a front end web dev.
             </Card.Text>
           </Card.Body>
-          <Card.Body>
+          <Card.Footer>
             <Card.Link href="https://github.com/mel-johnston"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/mel-johnston"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
-          </Card.Body>
+          </Card.Footer>
         </Card>
         <Card className="card" style={{ width: '20rem' }}>
           <Card.Img variant="top" src={jordanPhoto} />
@@ -48,10 +48,10 @@ class Profile extends Component {
               My name is Jordan Covington. I am from Campobello, South Carolina and am currently living at Joint Base Lewis-McChord, Washington with my wife, who is active duty Air Force, and my 2 daughters. I have a degree in Computer Information Systems and Business Administration from McKendree University. I am currently enrolled at Code Fellows to transition my career into web development.
             </Card.Text>
           </Card.Body>
-          <Card.Body>
+          <Card.Footer>
             <Card.Link href="https://github.com/JMCov"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/jordan-covington-05583a257/"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
-          </Card.Body>
+          </Card.Footer>
         </Card>
         <Card className="card" style={{ width: '20rem' }}>
           <Card.Img variant="top" src={marcusPhoto} />
@@ -61,10 +61,10 @@ class Profile extends Component {
               Veteran with a background in logistics and a passion for video games & technology. After working in logistics, I've decided to take the opportunity to switch careers into an industry I've always had an interest in. I'm always looking up and reading about new technology and would love to be "in the know" by working on new tech.
             </Card.Text>
           </Card.Body>
-          <Card.Body>
+          <Card.Footer>
             <Card.Link href="https://github.com/marcusbynoe"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/marcusbynoe"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
-          </Card.Body>
+          </Card.Footer>
         </Card>
         <Card className="card" style={{ width: '20rem' }}>
           <Card.Img variant="top" src={tylerPhoto} />
@@ -74,10 +74,10 @@ class Profile extends Component {
               I'm an Air Force veteran and aspiring software developer. I'm excited to learn how to build useful things and make them look beautiful too.
             </Card.Text>
           </Card.Body>
-          <Card.Body>
-            <Card.Link href="https://github.com/tyler-bennett52"><i className="icons-link fa-brands fa-github"></i></Card.Link>
+          <Card.Footer>
+            <Card.Link   href="https://github.com/tyler-bennett52"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/tyler-bennett-608928108/"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
-          </Card.Body>
+          </Card.Footer>
         </Card>
       </div>
     );
