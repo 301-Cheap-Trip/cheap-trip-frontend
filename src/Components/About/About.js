d6104454ae60b236acd3b87fb3848617550ca7b6
MELANIE JOHNSTON <MELANIE.JOHNSTON.1@OUTLOOK.COM>
2023-02-03 13:35:37 +0000

    about cards less bright

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,7 +18,7 @@ class Profile extends Component {
           <Card.Body>
             <Card.Title>Dominick Martin</Card.Title>
             <Card.Text className="card-text">
-               With a strong foundation in programming languages and experience working on projects in a team environment, Dominick is eager to bring his skills and enthusiasm to a new role as a software developer. He is confident in his ability to learn quickly and is dedicated to continuously improving his abilities to deliver high-quality software. -ChatGPT
+              With a strong foundation in programming languages and experience working on projects in a team environment, Dominick is eager to bring his skills and enthusiasm to a new role as a software developer. He is confident in his ability to learn quickly and is dedicated to continuously improving his abilities to deliver high-quality software. -ChatGPT
             </Card.Text>
           </Card.Body>
           <Card.Body>
@@ -47,6 +47,8 @@ class Profile extends Component {
             <Card.Text className="card-text">
               My name is Jordan Covington. I am from Campobello, South Carolina and am currently living at Joint Base Lewis-McChord, Washington with my wife, who is active duty Air Force, and my 2 daughters. I have a degree in Computer Information Systems and Business Administration from McKendree University. I am currently enrolled at Code Fellows to transition my career into web development.
             </Card.Text>
+          </Card.Body>
+          <Card.Body>
             <Card.Link href="https://github.com/JMCov"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/jordan-covington-05583a257/"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
           </Card.Body>
@@ -56,8 +58,10 @@ class Profile extends Component {
           <Card.Body>
             <Card.Title>Marcus Bynoe</Card.Title>
             <Card.Text className="card-text">
-              Veteran with a background in logistics and a passion for video games & technology. After working in logistics, I've decided to take the opportunity to switch carrers into an industry I've always had an interest in. I'm always looking up and reading about new technology and would love to be "in the know" by working on new tech.
+              Veteran with a background in logistics and a passion for video games & technology. After working in logistics, I've decided to take the opportunity to switch careers into an industry I've always had an interest in. I'm always looking up and reading about new technology and would love to be "in the know" by working on new tech.
             </Card.Text>
+          </Card.Body>
+          <Card.Body>
             <Card.Link href="https://github.com/marcusbynoe"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/marcusbynoe"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
           </Card.Body>
@@ -69,15 +73,13 @@ class Profile extends Component {
             <Card.Text className="card-text">
               I'm an Air Force veteran and aspiring software developer. I'm excited to learn how to build useful things and make them look beautiful too.
             </Card.Text>
+          </Card.Body>
+          <Card.Body>
             <Card.Link href="https://github.com/tyler-bennett52"><i className="icons-link fa-brands fa-github"></i></Card.Link>
             <Card.Link href="https://www.linkedin.com/in/tyler-bennett-608928108/"><i class="icons-link fa-brands fa-linkedin"></i></Card.Link>
           </Card.Body>
         </Card>
       </div>
-
-
-
-
     );
   }
 }
